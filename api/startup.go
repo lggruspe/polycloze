a093740ca7635a07fdf883a216725699f7acd6ac
Levi Gruspe <mail.levig@gmail.com>
2022-12-02 17:21:26 +0800

    Generate list of words during startup

--- api/startup.go
+++ api/startup.go
@@ -4,9 +4,12 @@
 package api
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/lggruspe/polycloze/basedir"
 	"github.com/lggruspe/polycloze/database"
@@ -31,6 +34,7 @@ func Startup() {
 		log.Fatal("Couldn't find installed courses. Please visit https://github.com/lggruspe/polycloze/tree/main/python")
 	}
 
+	// Generate courses.json.
 	coursesJSON := filepath.Join(basedir.StateDir, "courses.json")
 	err := writeJSON(coursesJSON, map[string][]Course{
 		"courses": courses,
@@ -39,12 +43,61 @@ func Startup() {
 		log.Fatal("failed to write courses.json:", err)
 	}
 
+	// Generate languages.json
 	languagesJSON := filepath.Join(basedir.StateDir, "languages.json")
 	err = writeJSON(languagesJSON, map[string][]Language{
 		"languages": languages,
 	})
 	if err != nil {
 		log.Fatal("failed to write languages.json:", err)
+	}
+
+	// Generate word lists.
+	path := filepath.Join(basedir.DataDir, "words")
+	if err := os.MkdirAll(path, 0777); err != nil {
+		log.Fatal("failed to get list of words:", err)
+	}
+
+	for _, course := range courses {
+		db, err := database.Open(basedir.Course(course.L1.Code, course.L2.Code))
+		if err != nil {
+			log.Fatal("failed to get list of words:", err)
+		}
+		defer db.Close()
+
+		query := `SELECT id, word, frequency_class FROM word`
+		rows, err := db.Query(query)
+		if err != nil {
+			log.Fatal("failed to get list of words:", err)
+		}
+		defer rows.Close()
+
+		f, err := os.Create(
+			filepath.Join(
+				path,
+				fmt.Sprintf("%s-%s.csv", course.L1.Code, course.L2.Code),
+			),
+		)
+		if err != nil {
+			log.Fatal("failed to get list of words:", err)
+		}
+		defer f.Close()
+
+		w := csv.NewWriter(f)
+		if err := w.Write([]string{"id", "word", "frequency_class"}); err != nil {
+			log.Fatal("failed to get list of words:", err)
+		}
+
+		for rows.Next() {
+			var word string
+			var id, frequencyClass int
+			if err := rows.Scan(&id, &word, &frequencyClass); err != nil {
+				log.Fatal("failed to get list of words:", err)
+			}
+			if err := w.Write([]string{strconv.Itoa(id), word, strconv.Itoa(frequencyClass)}); err != nil {
+				log.Fatal("failed to get list of words:", err)
+			}
+		}
 	}
 }
 
